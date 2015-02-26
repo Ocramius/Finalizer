90452eac685ba0cd20ede9da16451960307e4db1
Marco Pivetta <ocramius@gmail.com>
2015-02-26 22:11:12 +0000

    Adding class that cannot be made final because it does not implement any interface

---
+++ test/FinalizerTestAsset/NonFinalizableClassWithNoMethods.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace FinalizerTestAsset;
+
+/**
+ * A class that cannot be made final because:
+ *
+ *  - no interface is being implemented
+ */
+class NonFinalizableClassWithNoMethods
+{
+}
