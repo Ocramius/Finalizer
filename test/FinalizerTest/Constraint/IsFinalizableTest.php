f5de1d34d8901a871aedc9b05c0b6dd686ddef15
Marco Pivetta <ocramius@gmail.com>
2015-02-26 20:03:56 +0000

    Adding basic `IsFinalizable` constraint tests

--- test/FinalizerTest/Constraint/IsFinalizableTest.php
+++ test/FinalizerTest/Constraint/IsFinalizableTest.php
@@ -2,7 +2,35 @@
 
 namespace FinalizerTest\Constraint;
 
+use Finalizer\Constraint\IsFinalizable;
+
 class IsFinalizableTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @dataProvider finalizableClassesProvider
+     *
+     * @param \ReflectionClass   $class
+     * @param \ReflectionClass[] $definedClasses
+     * @param bool               $expected
+     */
+    public function testIsFinalizable(\ReflectionClass $class, array $definedClasses, $expected)
+    {
+        $this->assertSame($expected, (new IsFinalizable())->__invoke($class, ...$definedClasses));
+    }
 
+    /**
+     * @return \ReflectionClass[][]|\ReflectionClass[][][]
+     */
+    public function finalizableClassesProvider()
+    {
+        return [
+            [
+                new \ReflectionClass(__CLASS__),
+                [
+                    new \ReflectionClass(__CLASS__),
+                ],
+                false,
+            ],
+        ];
+    }
 }
