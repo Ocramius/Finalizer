a3fbd062339aa0c9a6b94d2c27f987bda8b39862
Marco Pivetta <ocramius@gmail.com>
2015-02-26 23:38:49 +0000

    An invokable with a constructor should be considered finalizable

---
+++ test/FinalizerTestAsset/Finalizable/InvokableClassWithConstructor.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace FinalizerTestAsset\Finalizable;
+
+/**
+ * A class that can be made final and acts as an invokable, but also has a constructor
+ */
+class InvokableClassWithConstructor
+{
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function __invoke()
+    {
+    }
+}
