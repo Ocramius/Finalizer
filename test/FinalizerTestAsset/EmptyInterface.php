edbd8afab314e723261ecf02720839015f492fe9
Marco Pivetta <ocramius@gmail.com>
2015-02-26 22:11:39 +0000

    Adding simple empty finalizable class

---
+++ test/FinalizerTestAsset/EmptyInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FinalizerTestAsset;
+
+/**
+ * An interface with no methods
+ */
+interface EmptyInterface
+{
+}
---
+++ test/FinalizerTestAsset/FinalizableClassWithNoMethods.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FinalizerTestAsset;
+
+/**
+ * A class that can be made final and has no methods
+ */
+class FinalizableClassWithNoMethods implements EmptyInterface
+{
+}
