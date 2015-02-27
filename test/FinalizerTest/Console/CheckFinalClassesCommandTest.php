95d982bd7da974a0cbaab2905dc6da19db400200
Marco Pivetta <ocramius@gmail.com>
2015-02-27 02:46:59 +0000

    Coverage for the provided CLI commands

---
+++ test/FinalizerTest/Console/CheckFinalClassesCommandTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace FinalizerTest\Console;
+
+use Finalizer\Console\CheckFinalClassesCommand;
+use FinalizerTestAsset\Finalizable;
+use FinalizerTestAsset\NonFinalizable;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Tests\Fixtures\DummyOutput;
+
+/**
+ * @covers \Finalizer\Console\CheckFinalClassesCommand
+ */
+class CheckFinalClassesCommandTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider pathsProvider
+     *
+     * @param string[] $paths
+     * @param string   $expectedOutput
+     */
+    public function testCheckFinalClassesCommand(array $paths, $expectedOutput)
+    {
+        $output = new DummyOutput();
+
+        (new CheckFinalClassesCommand())->run(new ArrayInput(['directories' => $paths]), $output);
+
+        $outputString = $output->fetch();
+
+        $this->assertEquals($expectedOutput, $outputString);
+    }
+
+    /**
+     * Data provider
+     *
+     * @return array
+     */
+    public function pathsProvider()
+    {
+        return [
+            [
+                [__DIR__ . '/../../../src'],
+                '',
+            ],
+            [
+                [__DIR__ . '/../../FinalizerTestAsset'],
+                <<<'OUTPUT'
+Following classes need to be made final:
++--------------------------------------------------------------+
+| FinalizerTestAsset\Finalizable\ClassWithConstructor          |
+| FinalizerTestAsset\Finalizable\ClassWithNoMethods            |
+| FinalizerTestAsset\Finalizable\ClassWithPrivateMethod        |
+| FinalizerTestAsset\Finalizable\ClassWithProtectedMethod      |
+| FinalizerTestAsset\Finalizable\EmptyChildClass               |
+| FinalizerTestAsset\Finalizable\FooBarConstructorMethodClass  |
+| FinalizerTestAsset\Finalizable\FooBarMethodClass             |
+| FinalizerTestAsset\Finalizable\FooMethodClass                |
+| FinalizerTestAsset\Finalizable\InvokableClass                |
+| FinalizerTestAsset\Finalizable\InvokableClassWithConstructor |
++--------------------------------------------------------------+
+Following classes are final and need to be made extensible again:
++-----------------------------------------------------------+
+| FinalizerTestAsset\InvalidFinal\ClassThatShouldNotBeFinal |
++-----------------------------------------------------------+
+
+OUTPUT
+            ],
+            [
+                [__DIR__ . '/../../../src', __DIR__ . '/../../FinalizerTestAsset'],
+                <<<'OUTPUT'
+Following classes need to be made final:
++--------------------------------------------------------------+
+| FinalizerTestAsset\Finalizable\ClassWithConstructor          |
+| FinalizerTestAsset\Finalizable\ClassWithNoMethods            |
+| FinalizerTestAsset\Finalizable\ClassWithPrivateMethod        |
+| FinalizerTestAsset\Finalizable\ClassWithProtectedMethod      |
+| FinalizerTestAsset\Finalizable\EmptyChildClass               |
+| FinalizerTestAsset\Finalizable\FooBarConstructorMethodClass  |
+| FinalizerTestAsset\Finalizable\FooBarMethodClass             |
+| FinalizerTestAsset\Finalizable\FooMethodClass                |
+| FinalizerTestAsset\Finalizable\InvokableClass                |
+| FinalizerTestAsset\Finalizable\InvokableClassWithConstructor |
++--------------------------------------------------------------+
+Following classes are final and need to be made extensible again:
++-----------------------------------------------------------+
+| FinalizerTestAsset\InvalidFinal\ClassThatShouldNotBeFinal |
++-----------------------------------------------------------+
+
+OUTPUT
+            ],
+            [
+                [
+                    __DIR__ . '/../../FinalizerTestAsset/Finalizable',
+                    __DIR__ . '/../../FinalizerTestAsset/InvalidFinal',
+                ],
+                <<<'OUTPUT'
+Following classes need to be made final:
++--------------------------------------------------------------+
+| FinalizerTestAsset\Finalizable\ClassWithConstructor          |
+| FinalizerTestAsset\Finalizable\ClassWithNoMethods            |
+| FinalizerTestAsset\Finalizable\ClassWithPrivateMethod        |
+| FinalizerTestAsset\Finalizable\ClassWithProtectedMethod      |
+| FinalizerTestAsset\Finalizable\EmptyChildClass               |
+| FinalizerTestAsset\Finalizable\FooBarConstructorMethodClass  |
+| FinalizerTestAsset\Finalizable\FooBarMethodClass             |
+| FinalizerTestAsset\Finalizable\FooMethodClass                |
+| FinalizerTestAsset\Finalizable\InvokableClass                |
+| FinalizerTestAsset\Finalizable\InvokableClassWithConstructor |
++--------------------------------------------------------------+
+Following classes are final and need to be made extensible again:
++-----------------------------------------------------------+
+| FinalizerTestAsset\InvalidFinal\ClassThatShouldNotBeFinal |
++-----------------------------------------------------------+
+
+OUTPUT
+            ],
+        ];
+    }
+}
